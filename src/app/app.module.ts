69a26effab41a146e5c2dacf027cf841bf9fd082
gregdorian <greg_dorian@yahoo.com>
2020-03-06 09:14:38 -0500

    1 file modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { RsidebarComponent } from './UIView/rsidebar/rsidebar.component';
-import { EncabezadoComponent } from './UIView/StaticComponents/encabezado/encabezado.component';
-import { FooterComponent } from './UIView/StaticComponents/footer/footer.component';
-import { AdministracionComponent } from './FrontModules/administracion/administracion.component';
-import { CalculosComponent } from './FrontModules/calculos/calculos.component';
-import { ConsultasComponent } from './FrontModules/consultas/consultas.component';
-import { PlanReduccionComponent } from './FrontModules/plan-reduccion/plan-reduccion.component';
+//import { EncabezadoComponent } from './UIView/StaticComponents/encabezado/encabezado.component';
+//import { FooterComponent } from './UIView/StaticComponents/footer/footer.component';
+//import { AdministracionComponent } from './FrontModules/administracion/administracion.component';
+//import { CalculosComponent } from './FrontModules/calculos/calculos.component';
+//import { ConsultasComponent } from './FrontModules/consultas/consultas.component';
+//import { PlanReduccionComponent } from './FrontModules/plan-reduccion/plan-reduccion.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, RsidebarComponent, EncabezadoComponent, FooterComponent, AdministracionComponent, CalculosComponent, ConsultasComponent, PlanReduccionComponent ],
+  declarations: [ 
+    AppComponent, 
+    RsidebarComponent,
+    //EncabezadoComponent,
+    //FooterComponent,
+    //AdministracionComponent,
+    //CalculosComponent,
+    //ConsultasComponent,
+    //PlanReduccionComponent 
+    ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
